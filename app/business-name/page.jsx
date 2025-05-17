eba3a74444a7f439e0e6e77cf7fd119839a21390
Shaqy2024 <sakib950006@gmail.com>
2025-05-17 23:09:26 +0530

    comited

--- app/business-name/page.jsx
+++ app/business-name/page.jsx
@@ -1,8 +1,18 @@
+import Sidebar from '@/components/Sidebar'
 import React from 'react'
 
 const BusinessName = () => {
   return (
-    <div>page</div>
+    <div className='px-20'>
+      <h1 className='mt-10 text-4xl text-white font-semibold text-center border-b pb-10 border-gray-500'>Business Name</h1>
+  
+  <div className='flex justify-center gap-10 mt-10'>
+    <div className='w-[25%] '>
+      <Sidebar />
+    </div>
+    <div className='w-[75%] border'></div>
+    </div> 
+    </div>
   )
 }
 
